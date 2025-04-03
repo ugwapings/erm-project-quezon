0c832af663bb6b8ec29fcf9e55d13cb1bd0a8f12
Angelou Lapad <ugwapings@gmail.com>
2025-04-03 15:59:25 +0800

    add option color in dark mode

--- resources/views/livewire/employee-categories.blade.php
+++ resources/views/livewire/employee-categories.blade.php
@@ -1,22 +1,22 @@
 <div class="grid md:grid-cols-3 grid-cols-1 gap-2 mb-4">
     <select wire:model.change="classification" class="border border-gray-300 rounded-md p-2">
-        <option value="">All Classifications</option>
-        <option value="Job Order">Job Order</option>
-        <option value="Regular">Regular</option>
-        <option value="Casual">Casual</option>
-        <option value="Honorarium">Honorarium</option>
-        <option value="Co-Terminus">Co-Terminus</option>
+        <option class="dark:bg-gray-800" value="">All Classifications</option>
+        <option class="dark:bg-gray-800" value="Job Order">Job Order</option>
+        <option class="dark:bg-gray-800" value="Regular">Regular</option>
+        <option class="dark:bg-gray-800" value="Casual">Casual</option>
+        <option class="dark:bg-gray-800" value="Honorarium">Honorarium</option>
+        <option class="dark:bg-gray-800" value="Co-Terminus">Co-Terminus</option>
     </select>
     <select wire:model.change="position" class="border border-gray-300 rounded-md p-2">
-        <option value="">All Positions</option>
+        <option class="dark:bg-gray-800" value="">All Positions</option>
         @foreach($positions as $position)
-            <option value="{{ $position->id }}">{{ $position->position_name }}</option>
+            <option class="dark:bg-gray-800" value="{{ $position->id }}">{{ $position->position_name }}</option>
         @endforeach
     </select>
     <select wire:model.change="office" class="border border-gray-300 rounded-md p-2">
-        <option value="">All Offices</option>
+        <option class="dark:bg-gray-800" value="">All Offices</option>
         @foreach($offices as $office)
-            <option value="{{ $office->id }}">{{ $office->office_name }}</option>
+            <option class="dark:bg-gray-800" value="{{ $office->id }}">{{ $office->office_name }}</option>
         @endforeach
     </select>
 </div>
